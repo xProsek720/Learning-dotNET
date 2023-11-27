183420bcc88f8fe15baa4561cbe5b14e0753d243
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-11-27 19:20:18 +0100

    v1 Learning .NetFramework

---
+++ API/Program.cs
@@ -0,0 +1,85 @@
+using API.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+
+        var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/pogoda", () =>
+            {
+                var forecast =  Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 3),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+
+
+// Auto oczyszczajacy sie scope
+        using var scope = app.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+            app.MapGet("/api/activities/{Id}", (Guid ID) =>
+                new ActivitiesController(context).GetActivity(ID));
+            app.MapGet("/api/activities", () =>
+                new ActivitiesController(context).GetActivities());
+        }
+        catch (Exception e)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError($"{e} occured during migration");
+
+        }
+
+
+        app.Run();
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
