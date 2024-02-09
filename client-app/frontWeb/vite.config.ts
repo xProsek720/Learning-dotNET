43e664ad2ea28461572719421fd656a77b03117c
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2024-02-09 23:14:19 +0100

    v1.1

--- client-app/frontWeb/vite.config.ts
+++ client-app/frontWeb/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80
+    port: 80,
   },
   plugins: [react()],
-})
+  build: {
+    outDir: "site/wwwroot/",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+});
