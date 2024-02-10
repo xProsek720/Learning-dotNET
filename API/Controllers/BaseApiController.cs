e6f85b5d50f953979c34cf79135d345ace463846
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2024-02-10 01:58:35 +0100

    v1.1 BAC

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("/api/[controller]")]
 
 public class BaseApiController : ControllerBase
 {
